60dd0a49ba20b1226681b4f244d5e56aaa1b3800
demo1 <2096667233@qq.com>
2016-12-22 15:31:31 +0800

    done

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # ericzheng_less_tool
 this is a less tool for convert less files into css files.
 
+# for
+
+上海黑马前端双元一期
+
 # Install
 ```
 npm init -y
@@ -8,11 +12,13 @@ npm install --save ericzheng_less_tool --save
 ```
 
 # Usages
-1. create a directory like src in the folders
-2. take all your less files in the folds
+1. create a directory named `src` in the root folder of your project
+2. take all your less files in the folds of `src`
 3. use the directive `npm build`
+4. enjoy your css files in the folder of 
 
-# Build
+# notice
+if you want to change the name of `src`,`dist`,please edit the package.json file
 
 # License
 this tool is released under the MIT License,but you can copy and use it as you like ~~
--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-let less = require('less');
+const fs = require('fs');
+const path = require('path');
+const less = require('less');
+const notifier = require('node-notifier');
+const tlog = require('talog');
+
+//node a.js source-dir src dist-dir dist
+let arr = process.argv;
+let sourceDir = arr[3];
+let distDir   = arr[5];
+fs.readdir(path.join(__dirname,sourceDir),(err,files)=>{
+  if(err){
+    notifier.notify({
+      'title':'error',
+      'sound':true,
+      'icon':path.join(__dirname,'eric.png'),
+      //O(∩_∩)O
+      'message':'CMD里输入的命令有问题~~~'
+    });
+  }else{
+    if(files.length > 0){
+      files.forEach((fileName,index)=>{
+        if(fileName.slice(-5) === '.less'){
+          fs.readFile(path.join(__dirname,sourceDir,fileName),'utf-8',(err,data)=>{
+            if(err){
+              throw err;
+            }
+            less.render(data,(err,output)=>{
+              if(err){
+                throw err;
+              }
+              fs.writeFile(path.join(__dirname,distDir,fileName.replace('.less','.css')),output.css,(err)=>{
+                if(err){
+                  throw err;
+                }
+                tlog('[I] 成功编译less文件"%s"', fileName);
+              });
+            })
+          });
+        }
+      });
+    }
+  }
+});
